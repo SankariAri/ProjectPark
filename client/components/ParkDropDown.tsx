e97614af7b5b3d685a4ecbd8a9d84d6545ed670a
Sankari Ayyaluru <sankariayyaluru@gmail.com>
2022-08-13 22:21:42 -0700

    configured park drop down

--- client/components/ParkDropDown.tsx
+++ client/components/ParkDropDown.tsx
@@ -3,12 +3,14 @@ import parkData from '../data/parkdata';
 
 
 const ParkDropDown = () => {
+    const parkNames = Object.keys(parkData)
     return (
     <div className ='dropdown'>
         <h3>Select a Park: </h3>
        <select>
-           <option value = "yosemite"> Yosemite National Park</option>
-           <option value = "yellowstone"> Yellowstone National Park</option>    
+        {parkNames.map((park) => (
+            <option value = {park}> {park} </option>
+        ))}
        </select> 
     </div>
     )
--- client/data/parkdata.ts
+++ client/data/parkdata.ts
@@ -1,15 +1,26 @@
 const parkData = {
     Yellowstone: { hikes: [
-        {name:'Fairy Falls Trail', latitude:44.7341037505, longitude: -110.8324 },
-        {name: 'Cascade Canyon Trail', latitude: 43.767190, longitude: -110.744270},
-        {name: 'Grand Prismatic Hot Spring', latitude: 44.515430, longitude: -110.832510},
+        {name:'Fairy Falls Trail', latitude:44.7341037505, longitude:-110.8324 },
+        {name: 'Cascade Canyon Trail', latitude:43.767190, longitude:-110.744270},
+        {name: 'Grand Prismatic Hot Spring', latitude:44.515430, longitude:-110.832510},
     
     ]},
     Yosemite: { hikes: [
-        {},
-        {},
-        {},
-    ]}
+        {name: 'Lower Yosemite Falls', latitude:37.746110, longitude:-119.596920},
+        {name: 'Vernal Falls', latitude:37.732520, longitude:-119.559400},
+        {name: 'Mirror Lake', latitude:37.739490, longitude:-119.559790},
+    ]},
+    Lassen: {hikes: [
+        {name: 'Lassen Peak', lattitude:40.474580, longitude:-121.505700},
+        {name: 'Bumpass Hell', lattitude:40.466100, longitude:-121.514050},
+        {name: 'Kings Creek Falls Trail', lattitude:40.460600, longitude:-121.459380},
+    ]},
+    Zion: {hikes:[
+        {name: 'Angels Landing Trail', lattitude:37.259090, longitude:-112.951220},
+        {name:'Zion Narrows Riverside Walk', lattitude:37.285160, longitude:-112.947620},
+        {name:'Emerald Pools Trail', lattitude:37.251790, longitude:-112.956220},
+    ]},
+  
 }
 
 export default parkData
